8c70e49921ea835e87a17f9d5d88d8fd16916dba
nyqykk <1327719263@qq.com>
2020-08-24 13:23:59 +0800

    'update'

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+/*解决跨域问题*/
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,9 @@ router.post('/register', (req, res) => {
     if(boss){
       res.send({code:1, msg: '用户已存在'})
     } else{
-      new Boss({username, type, password}).save((err, bossDoc) => {
-        const data = {username, type}
+      new Boss({username, type, password}).save((err, user) => {
+        res.cookie('userId', user._id, {maxAge: 1000*60*60*24})
+        const data = {username, type, _id: user._id}
         res.send({code:0, data: data})
       })
     }
@@ -22,11 +23,32 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-  Boss.findOne(req.body, (err, tarBoss) => {
-    if(tarBoss)
-      res.send({code:0, data: tarBoss})
+  Boss.findOne(req.body, (err, user) => {
+    if(user){
+      res.cookie('userId', user._id, {maxAge: 1000*60*60*24})
+      res.send({code:0, data: user})
+    }
     else
       res.send({code:1, msg:'用户名或密码不正确'})
+  })
+})
+
+router.post('/update', (req, res) => {
+  const user = req.body
+  const _id = req.cookies.userId
+  console.log(_id)
+  if(!_id){
+    return res.send({code:1, msg: '请先登录'})
+  }
+  Boss.findByIdAndUpdate(_id, user, (err, oldUser) => {
+    if(!oldUser){
+      res.clearCookie('userId')
+      res.send({code:1, msg: '请先登录'})
+    }else{
+      const {type, username, _id} = oldUser
+      let data = Object.assign(user, {type, username, _id})
+      res.send({code: 0, data})
+    }
   })
 })
 
